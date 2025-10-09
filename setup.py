f68ad8f3248a3b777e2db3486289c265b0a24e1d
Artasov <ivanhvalevskey@gmail.com>
2025-10-09 05:20:46 +0300

    process

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='xldashboard',
-    version='0.1.5',
+    version='0.1.6',
     author='xlartas',
     author_email='ivanhvalevskey@gmail.com',
     description='More beautiful/customizable admin dashboard for Django',
